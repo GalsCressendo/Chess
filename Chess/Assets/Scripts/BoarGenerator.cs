8ca65da82c309a37feb5992f49ee36941702d141
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2022-12-20 20:20:57 +0700

    spawn chess piece

--- Chess/Assets/Scripts/BoarGenerator.cs
+++ Chess/Assets/Scripts/BoarGenerator.cs
@@ -18,9 +18,11 @@ public class BoarGenerator : MonoBehaviour
     [Header("Pieces Components")]
     [SerializeField] private GameObject[] chessPiecePrefabs;
     [SerializeField] private Material[] colorMaterial;
+    private ChessPiece[,] chessPieces;
 
     private void Awake()
     {
+        SpawnAllChessPieces();
         GenerateAllTiles();
     }
 
@@ -61,7 +63,64 @@ public class BoarGenerator : MonoBehaviour
                 tile.transform.SetParent(transform, false);
                 tile.name = string.Format("X:{0},Y:{1}", x, y);
                 tile.tag = TILE_TAG;
+
+                RepositionChessPiece(x, y, tile);
             }
+        }
+
+    }
+
+    private void SpawnAllChessPieces()
+    {
+        chessPieces = new ChessPiece[TILE_X_COUNT, TILE_Y_COUNT];
+
+        int blackTeam = 1, whiteTeam = 0;
+
+        //White Team
+        chessPieces[0, 7] = SpawnSinglePiece(ChessPieceType.Rook, whiteTeam);
+        chessPieces[1, 7] = SpawnSinglePiece(ChessPieceType.Knight, whiteTeam);
+        chessPieces[2, 7] = SpawnSinglePiece(ChessPieceType.Bishop, whiteTeam);
+        chessPieces[3, 7] = SpawnSinglePiece(ChessPieceType.Queen, whiteTeam);
+        chessPieces[4, 7] = SpawnSinglePiece(ChessPieceType.King, whiteTeam);
+        chessPieces[5, 7] = SpawnSinglePiece(ChessPieceType.Bishop, whiteTeam);
+        chessPieces[6, 7] = SpawnSinglePiece(ChessPieceType.Knight, whiteTeam);
+        chessPieces[7, 7] = SpawnSinglePiece(ChessPieceType.Rook, whiteTeam);
+        for(int i=0; i < TILE_X_COUNT; i++)
+        {
+            chessPieces[i, 6] = SpawnSinglePiece(ChessPieceType.Pawn, whiteTeam);
+        }
+
+        //Black Team
+        chessPieces[0, 0] = SpawnSinglePiece(ChessPieceType.Rook, blackTeam);
+        chessPieces[1, 0] = SpawnSinglePiece(ChessPieceType.Knight,blackTeam);
+        chessPieces[2, 0] = SpawnSinglePiece(ChessPieceType.Bishop, blackTeam);
+        chessPieces[3, 0] = SpawnSinglePiece(ChessPieceType.Queen, blackTeam);
+        chessPieces[4, 0] = SpawnSinglePiece(ChessPieceType.King, blackTeam);
+        chessPieces[5, 0] = SpawnSinglePiece(ChessPieceType.Bishop, blackTeam);
+        chessPieces[6, 0] = SpawnSinglePiece(ChessPieceType.Knight, blackTeam);
+        chessPieces[7, 0] = SpawnSinglePiece(ChessPieceType.Rook, blackTeam);
+        for (int i = 0; i < TILE_X_COUNT; i++)
+        {
+            chessPieces[i, 1] = SpawnSinglePiece(ChessPieceType.Pawn, blackTeam);
+        }
+
+    }
+
+    private ChessPiece SpawnSinglePiece(ChessPieceType type, int team)
+    {
+        ChessPiece cp = Instantiate(chessPiecePrefabs[(int)type - 1], transform).GetComponent<ChessPiece>();
+        cp.type = type;
+        cp.team = team;
+        cp.GetComponent<MeshRenderer>().material = colorMaterial[team];
+
+        return cp;
+    }
+
+    private void RepositionChessPiece(int x, int y, GameObject parent)
+    {
+        if (chessPieces[x, y] != null)
+        {
+            chessPieces[x, y].transform.SetParent(parent.transform, false);
         }
 
     }
