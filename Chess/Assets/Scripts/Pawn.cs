40a00b6aa0ef6550a97f16a6d51f3fcb3198abef
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2022-12-22 17:58:22 +0700

    fix pawn issue

--- Chess/Assets/Scripts/Pawn.cs
+++ Chess/Assets/Scripts/Pawn.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,17 +9,23 @@ public class Pawn : ChessPiece
 
         int direction = (team == 0) ? 1 : -1; //up if white, down if black
 
+        //Check if pawn on top or bottom of the board
+        if ((team == 0 && currentY == 7) || (team == 1 && currentY == 0))
+        {
+            return r;
+        }
+
         //One in front
-        if(board[currentX,currentY + direction] == null)
+        if (board[currentX, currentY + direction] == null)
         {
             r.Add(new Vector2Int(currentX, currentY + direction));
         }
 
         //Two in front (in initial position)
-        if(board[currentX,currentY + direction] == null)
+        if (board[currentX, currentY + direction] == null)
         {
             //Team white direction
-            if(team == 0 && currentY ==1 && board[currentX, currentY + (direction *2)] == null)
+            if (team == 0 && currentY == 1 && board[currentX, currentY + (direction * 2)] == null)
             {
                 r.Add(new Vector2Int(currentX, currentY + (direction * 2)));
             }
@@ -33,9 +38,9 @@ public class Pawn : ChessPiece
         }
 
         //Kill Move
-        if(currentX != tileCountX - 1) //if not on the most right side of the board
+        if (currentX != tileCountX - 1) //if not on the most right side of the board
         {
-            if(board[currentX + 1, currentY+direction]!=null && board[currentX + 1, currentY + direction].team != team)
+            if (board[currentX + 1, currentY + direction] != null && board[currentX + 1, currentY + direction].team != team)
             {
                 r.Add(new Vector2Int(currentX + 1, currentY + direction));
             }
@@ -57,7 +62,7 @@ public class Pawn : ChessPiece
 
         //Promotion
         //check pawn team and the position of the pawn
-        if((team == 0 && currentY ==6) || (team == 1 && currentY == 1))
+        if ((team == 0 && currentY == 6) || (team == 1 && currentY == 1))
         {
             return BoardManager.SpecialMove.Promotion;
         }
@@ -68,23 +73,23 @@ public class Pawn : ChessPiece
         {
             var lastMove = moveList[moveList.Count - 1];
             //if lastMove done by a pawn
-            if(board[lastMove[1].x, lastMove[1].y].type == ChessPieceType.Pawn)
+            if (board[lastMove[1].x, lastMove[1].y].type == ChessPieceType.Pawn)
             {
-                if(Mathf.Abs(lastMove[0].y - lastMove[1].y) == 2) //if the moved pawn at least move 2 units
+                if (Mathf.Abs(lastMove[0].y - lastMove[1].y) == 2) //if the moved pawn at least move 2 units
                 {
-                    if(board[lastMove[1].x, lastMove[1].y].team != team) //if the move was from the other team
+                    if (board[lastMove[1].x, lastMove[1].y].team != team) //if the move was from the other team
                     {
-                        if(lastMove[1].y == currentY) //if the opponet team pawn lands exactly to my right or my left
+                        if (lastMove[1].y == currentY) //if the opponet team pawn lands exactly to my right or my left
                         {
                             //left
-                            if(lastMove[1].x == currentX - 1)
+                            if (lastMove[1].x == currentX - 1)
                             {
                                 availableMoves.Add(new Vector2Int(currentX - 1, currentY + direction));
                                 return BoardManager.SpecialMove.EnPassant;
                             }
 
                             //right
-                            if(lastMove[1].x == currentX + 1)
+                            if (lastMove[1].x == currentX + 1)
                             {
                                 availableMoves.Add(new Vector2Int(currentX + 1, currentY + direction));
                                 return BoardManager.SpecialMove.EnPassant;
